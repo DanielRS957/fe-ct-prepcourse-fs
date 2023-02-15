9d20ecb0f2ea2ebf4449acf5df4bbd82373ba10b
DanielRS957 <drsp9501@icloud.com>
2023-02-15 18:54:33 -0500

    modulo 3

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,13 +5,24 @@ function sonIguales(x, y) {
    // Retorna True si "x" e "y" son iguales.
    // De lo contrario, retorna False.
    // Tu código:
+   if (x === y){
+   return true;
+}else {
+      return false;
+}
+
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna True si los dos strings tienen la misma longitud.
    // De lo contrario, retorna False.
    // Tu código:
+   if (str1.lenght === str2.lenght);
+   return true;
+} { return false;
 }
+
+
 
 function menosQueNoventa(num) {
    // Retorna True si el argumento "num" es menor que noventa.
